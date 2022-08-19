5e90158545ffe3f3dd808b4c078c2d7000e68967
Ronan Avila <avila.ronan@gmail.com>
2022-08-19 09:19:56 -0300

    WIP: Alteracao Layout

--- Shop.WEB/Controllers/HomeController.cs
+++ Shop.WEB/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Shop.WEB.Models;
+using Shop.WEB.Services.Interfaces;
 using System.Diagnostics;
 
 namespace Shop.WEB.Controllers
@@ -9,17 +10,33 @@ namespace Shop.WEB.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IProductService _productService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IProductService productService)
         {
             _logger = logger;
+            _productService = productService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var result = await _productService.GetAllProducts(string.Empty);
+
+            if (result is null)
+                return View("Error");
+
+            return View(result);
         }
 
+        [HttpGet]
+        public async Task<ActionResult<ProductViewModel>> ProductDetails(int id)
+        {
+            var result = await _productService.GetProductById(id, string.Empty);
+            if (result is null)
+                return View("Error");
+
+            return View(result);
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
